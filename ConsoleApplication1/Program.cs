c5b4f267e0b56f6da1cb923eeb04eb98400e920a
mahesaluthan <echasmith97@gmail.com>
2017-08-23 10:07:09 +0700

    console

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class ProgramPertama
+    {
+        public int x;
+        internal int y;
+    }
+}
---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class ProgramPertama
+    {
+        public int x;
+        internal int y;
+    }
+    class ProgramKedua
+    {
+        static void Main(string[] args)
+        {
+            ProgramPertama pu = new ProgramPertama();
+
+            pu.x = 3;
+            pu.y = 5;
+            Console.WriteLine("x = {0}, y = {1}", pu.x, pu.y);
+            Console.Read();
+        }
+    }
+}
